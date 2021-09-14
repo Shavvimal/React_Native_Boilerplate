5b26968879ba401f8cff02bca596c5d3c5e3e290
Shav <shavhugan@gmail.com>
2021-09-14 22:26:02 +0100

    added global styles

---
+++ src/GlobStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { fontSizes, spacing } from '../utils/sizes';
+
+export const GlobStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    text: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: fontSizes.lg
+    },
+});
